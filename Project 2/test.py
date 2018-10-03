1e86e62f783bf84ba454ae3e6f1affe85211535e
bluemoon0011 <xin.jin0011@gmail.com>
2018-10-03 13:50:22 -0400

    update project 2

---
+++ Project 2/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: Xin Jin
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@contact: xin.jin0010@gmail.com
+@software: pycharm
+@file: test.py
+@time: 10/3/18 1:49 PM
+@desc:
+'''
+
+import numpy
